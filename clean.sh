ac92eb93ac9c478cf40b928d4d5cdb71965ba5f8
DangSage <3thandang@gmail.com>
2024-03-21 13:08:15 -0400

    made clean shell script work w/ subdirs and pdf's

--- clean.sh
+++ clean.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+# This script deletes all LaTeX auxiliary files in the current directory and its subdirectories.
+# There is an option to delete pdf files as well.
 
-latexmk -c
+if [ "$1" == "pdf" ]; then
+    # List of LaTeX auxiliary file extensions
+    aux_files=("*.aux" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.log" "*.out" "*.pdf" "*.synctex.gz" "*.toc")
+else
+    aux_files=("*.aux" "*.bbl" "*.blg" "*.fdb_latexmk" "*.fls" "*.log" "*.out" "*.synctex.gz" "*.toc")
+fi
+
+# Loop over the file extensions and delete the files
+for ext in "${aux_files[@]}"; do
+    find . -type f -name "$ext" -delete
+done
